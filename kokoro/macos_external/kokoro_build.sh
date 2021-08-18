a6588bdafadfbbac353934d2121ac749ebb756d6
Nishchal Chandna <nishchalc@google.com>
2021-08-18 15:07:21 -0400

    Update kokoro_build.sh

--- kokoro/macos_external/kokoro_build.sh
+++ kokoro/macos_external/kokoro_build.sh
@@ -9,6 +9,5 @@ set -x
 # Code under repo is checked out to ${KOKORO_ARTIFACTS_DIR}/github.
 # The final directory name in this path is determined by the scm name specified
 # in the job configuration.
-cat ${KOKORO_GFILE_DIR}/test
 cd ${KOKORO_ARTIFACTS_DIR}/github/kokoro-codelab-nishchalc
 ./build.sh
